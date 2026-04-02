d9dfc995f1cae2864fc66b1005f881cf70d2dea7
Uktamova <konankita@gmail.com>
2026-04-02 18:21:18 +0900

    last version

--- src/features/movies/components/movie-search.tsx
+++ src/features/movies/components/movie-search.tsx
@@ -1,49 +1,43 @@
 "use client";
 
 import { Input } from "antd";
-import { useRouter, usePathname, useSearchParams } from "next/navigation";
-import { useState, useMemo, useEffect } from "react";
-import debounce from "lodash/debounce";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export const MovieSearch = () => {
   const router = useRouter();
-  const pathname = usePathname();
   const searchParams = useSearchParams();
 
-  const [inputValue, setInputValue] = useState(searchParams.get("query") || "");
+  const currentQuery = searchParams.get("query") || "";
+  const [inputValue, setInputValue] = useState(currentQuery);
 
-  const debouncedPush = useMemo(
-    () =>
-      debounce((term: string) => {
-        const params = new URLSearchParams(window.location.search);
-        if (term) {
-          params.set("query", term);
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (inputValue !== currentQuery) {
+        const params = new URLSearchParams(searchParams.toString());
+
+        if (inputValue) {
+          params.set("query", inputValue);
         } else {
           params.delete("query");
         }
-        params.set("page", "1"); // При новом поиске всегда на 1 страницу
-        router.push(`${pathname}?${params.toString()}`);
-      }, 500),
-    [pathname, router],
-  );
+        params.set("page", "1"); // Сбрасываем на 1 страницу при новом поиске
 
-  useEffect(() => {
-    return () => debouncedPush.cancel();
-  }, [debouncedPush]);
+        router.push(`?${params.toString()}`);
+      }
+    }, 600);
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const val = e.target.value;
-    setInputValue(val);
-    debouncedPush(val);
-  };
+    return () => clearTimeout(delayDebounceFn);
+  }, [inputValue, router, searchParams, currentQuery]);
 
   return (
-    <Input
-      placeholder="Type to search movies..."
-      value={inputValue}
-      onChange={onChange}
-      allowClear
-      style={{ marginBottom: "20px", maxWidth: "1000px", height: "40px" }}
-    />
+    <div style={{ marginBottom: "20px", width: "100%", maxWidth: "1000px" }}>
+      <Input
+        placeholder="Type to search..."
+        size="large"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+    </div>
   );
 };
